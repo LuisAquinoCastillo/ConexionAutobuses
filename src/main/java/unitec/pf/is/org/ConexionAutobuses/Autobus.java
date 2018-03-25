319514ac494f3f9d2a670e9e816eb1d4447b4437
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-03-25 14:06:56 -0600

    Domingo 25 de Marzo 2018 Actualizacion 2:06 pm

---
+++ src/main/java/unitec/pf/is/org/ConexionAutobuses/Autobus.java
@@ -0,0 +1,12 @@
+package unitec.pf.is.org.ConexionAutobuses;
+
+public class Autobus {
+
+    Integer idAutobus;
+    String marcaAutobus;
+    String tipoAutobus;
+    String numeroAsientos;
+
+    //Getters y setters
+
+}
