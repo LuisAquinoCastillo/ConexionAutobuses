a092e9f6048ac8fd4e660657d895733bcf972722
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-03-25 13:56:08 -0600

    Domingo 25 de Marzo del 2018 Actualizacion 1:56pm

---
+++ src/main/java/unitec/pf/is/org/ConexionAutobuses/ConexionAutobusesApplication.java
@@ -0,0 +1,12 @@
+package unitec.pf.is.org.ConexionAutobuses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexionAutobusesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexionAutobusesApplication.class, args);
+	}
+}
