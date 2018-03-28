7902de7f5bd1800f859784f00978b645f38ecc01
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-03-27 23:45:35 -0600

    Martes 27 de Marzo 2018 Actualizacion 11:45pm

--- src/main/java/unitec/pf/is/org/ConexionAutobuses/ControlAutobus.java
+++ src/main/java/unitec/pf/is/org/ConexionAutobuses/ControlAutobus.java
@@ -1,7 +1,11 @@
 package unitec.pf.is.org.ConexionAutobuses;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
@@ -14,14 +18,12 @@ public class ControlAutobus {
 
 
     // Metodo para buscar todos los registros
-    @CrossOrigin
     @RequestMapping(value = "/autobus", method = RequestMethod.GET, headers = {"Accept=application/json"})
     public ArrayList<Autobus> buscarTodos(){
         return (ArrayList<Autobus>)repoABus.findAll();
     }
 
     //Metodo para guardar
-    @CrossOrigin
     @RequestMapping(value = "/autobus/{marcaAutobus}/{tipoAutobus}/{numeroAsientos}", method = RequestMethod.POST, headers = {"Accept=application/json"})
     public Estatus guardarAutobus(@PathVariable String marcaAutobus, @PathVariable String tipoAutobus, @PathVariable String numeroAsientos){
         repoABus.save(new Autobus(marcaAutobus, tipoAutobus, numeroAsientos));
@@ -29,7 +31,6 @@ public class ControlAutobus {
     }
 
     //Metodo para actualizar
-    @CrossOrigin
     @RequestMapping(value = "/autobus/{idAutobus}/{marcaAutobus}/{tipoAutobus}/{numeroAsientos}", method = RequestMethod.PUT, headers = {"Accept=application/json"})
     public Estatus actualizar(@PathVariable String idAutobus, @PathVariable String marcaAutobus, @PathVariable String tipoAutobus, @PathVariable String numeroAsientos){
         repoABus.save(new Autobus(idAutobus,marcaAutobus,tipoAutobus,numeroAsientos));
@@ -37,7 +38,6 @@ public class ControlAutobus {
     }
 
     //Metodo para borrar registro
-    @CrossOrigin
     @RequestMapping(value = "/autobus/{idAutobus}", method = RequestMethod.DELETE, headers = {"Accept=application/json"})
     public Estatus borrarAutobus(@PathVariable String idAutobus){
         repoABus.delete(new Autobus(idAutobus));
