c3976f5237dce07ebb54c867c45c8cf97f0234f4
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-04-03 11:36:51 -0500

    Martes 3 de Abril 2018 Actualizacion 11:36am

--- src/main/java/unitec/pf/is/org/ConexionAutobuses/ControlConductor.java
+++ src/main/java/unitec/pf/is/org/ConexionAutobuses/ControlConductor.java
@@ -21,13 +21,13 @@ public class ControlConductor {
     }
 
     @CrossOrigin
-    @RequestMapping(value = {"/{idConductor}"},method = RequestMethod.GET, headers = {"Accept=application/json"})
+    @RequestMapping(value = {"{idConductor}"},method = RequestMethod.GET, headers = {"Accept=application/json"})
     public Optional<Conductor> buscarPorId(@PathVariable String idConductor){
         return repoConduc.findById(idConductor);
     }
 
     @CrossOrigin
-    @RequestMapping(value = {"/{nombre}/{apellido_pat}/{apellido_mat}/{edad}/{sexo}/{idAutobus}"},
+    @RequestMapping(value = {"{nombre}/{apellido_pat}/{apellido_mat}/{edad}/{sexo}/{idAutobus}"},
     method = RequestMethod.POST, headers = {"Accept=application/json"})
     public Estatus insertar(@PathVariable String nombre, @PathVariable String apellido_pat, @PathVariable String apelledp_mat,
                             @PathVariable String edad, @PathVariable String sexo, @PathVariable String idAutobus){
@@ -36,7 +36,7 @@ public class ControlConductor {
     }
 
     @CrossOrigin
-    @RequestMapping(value = {"/{idConductor}/{nombre}/{apellido_pat}/{apellido_mat}/{edad}/{sexo}/{idAutobus}"},
+    @RequestMapping(value = {"{idConductor}/{nombre}/{apellido_pat}/{apellido_mat}/{edad}/{sexo}/{idAutobus}"},
     method = RequestMethod.PUT, headers = {"Accept=application/json"})
     public Estatus actualizar(@PathVariable String idConductor, @PathVariable String nombre, @PathVariable String apellido_pat, @PathVariable String apelledp_mat,
                               @PathVariable String edad, @PathVariable String sexo, @PathVariable String idAutobus){
@@ -45,7 +45,7 @@ public class ControlConductor {
     }
 
     @CrossOrigin
-    @RequestMapping(value = {"/{id}"}, method = RequestMethod.DELETE, headers = {"Accept=application/json"})
+    @RequestMapping(value = {"{id}"}, method = RequestMethod.DELETE, headers = {"Accept=application/json"})
     public Estatus borrar(@PathVariable String id){
         repoConduc.delete(new Conductor(id));
         return new Estatus(true, "Borrado exitoso");
