dcac09e8aaf0189fea76cfaa8940c6b98d9c4dba
luisabrahamaquinocastillo <luis.aquino.135769@gmail.com>
2018-04-03 12:01:02 -0500

    Martes 3 de Abril 2018 Actualizacion 12:00pm

--- src/main/java/unitec/pf/is/org/ConexionAutobuses/ControlConductor.java
+++ src/main/java/unitec/pf/is/org/ConexionAutobuses/ControlConductor.java
@@ -2,15 +2,13 @@ package unitec.pf.is.org.ConexionAutobuses;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Optional;
 
-@Controller
-@EnableAutoConfiguration
+
+
 @RestController
 @CrossOrigin
 @RequestMapping(value="/api/conductor",method = RequestMethod.TRACE)
